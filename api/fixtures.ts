74241910a91439534127eb6c5077ca03459d17da
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-30 13:02:15 +0600

    Added fixtures.ts,usersSlice.ts. usersThunk.ts, store.ts, hooks.ts, types.d.ts

---
+++ api/fixtures.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import config from "./config";
+import User from "./models/User";
+
+
+const dropCollection = async (
+    db: mongoose.Connection,
+    collectionName: string,
+) => {
+    try {
+        await db.dropCollection(collectionName);
+    } catch (e) {
+        console.log(`Collection ${collectionName} was missing, skipping drop...`);
+    }
+};
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
+    const db = mongoose.connection;
+    const collections = ['users'];
+
+    for (const collectionName of collections) {
+        await dropCollection(db, collectionName);
+    }
+
+    const [user1, user2] = await User.create(
+        {
+            email: 'Zhanyl@gallery.local',
+            displayName: 'Zhanyl',
+            password: '123',
+            token: crypto.randomUUID(),
+            role: 'admin',
+            avatar: 'fixtures/avatar.png',
+        },
+        {
+            email: 'TestUser@gallery.local',
+            displayName: 'TestUser',
+            password: '000',
+            token: crypto.randomUUID(),
+            role: 'user',
+            avatar: "fixtures/avatar.png"
+        },
+    );
+
+    await db.close();
+};
+
+void run();
