50db29e5e3af3de4990e63eedd79f491855c50f3
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-30 13:17:39 +0600

    Added UI components,Login.tsx,Register.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,29 @@
+import ToolBar from './UI/ToolBar.tsx';
+import { Container, CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Home from './features/arts/Home.tsx';
+import Register from './features/users/Register.tsx';
+import Login from './features/users/Login.tsx';
 
 
 function App() {
 
   return (
     <>
-
+      <CssBaseline/>
+      <header>
+        <ToolBar/>
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/register" element={<Register />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="*" element={<h1>Not found</h1>} />
+          </Routes>
+        </Container>
+      </main>
     </>
   )
 }
